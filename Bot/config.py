468c4b614b1c008ae81e7dcf7daa14e126758ca1
ChauhanSaarthi <162335006+GodHeisen@users.noreply.github.com>
2025-01-21 16:39:16 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,7 @@
 # config.py
-api_id = "27159920"
-api_hash = "e9f3d68caf70d80587c0371cc9d35213"
-bot_token = "7572375796:AAHgpY5ehCXVQ3e-wejlLy2tYRShLFhSO4U"
-MONGO_URL = "mongodb+srv://ChauhanSaarthi:<Heisen*#0003>@cluster0.ip0hi.mongodb.net/"
+api_id = ""
+api_hash = ""
+bot_token = ""
+MONGO_URL = "mongodb+srv://:<>@cluster0.ip0hi.mongodb.net/"
 OWNER_ID = 7875121895
 SUPPORT_CHAT_ID = -1001995859442
